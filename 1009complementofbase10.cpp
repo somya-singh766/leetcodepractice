46d7787555c40bc3fde22628c48b6e11256a7488
somya-singh766 <Somyasingh3007@gmail.com>
2025-07-21 17:02:29 +0530

    Create 1009complementofbase10.cpp

---
+++ 1009complementofbase10.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n==0){
+            return 1;
+        }
+        int ans = 0 , mul = 1, rem;
+        while(n){
+            rem=n%2;
+            rem = rem^1;
+            n/=2;
+            ans = ans+rem*mul;
+            mul*=2;
+        }
+        return ans;
+    }
+};
