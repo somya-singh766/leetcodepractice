41a45b3de48a09ad562445015446713654336c18
somya-singh766 <Somyasingh3007@gmail.com>
2025-11-26 13:44:09 +0530

    Add solution for Coin Change II

--- LeetCode/Coin-Change-II/Solution.cpp
+++ LeetCode/Coin-Change-II/Solution.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    int solve(int i,int amount, vector<int>& coins,int n,vector<vector<int>>&dp)
+
+    int ways(int i,int amount,vector<int>& coins,vector<vector<int>>&dp)
     {
+        if(i<0)
+        return 0;
         if(amount==0)
         return 1;
-        if(amount<0 || i>=n)
-        return 0;
         if(dp[i][amount]!=-1)
         return dp[i][amount];
-        return dp[i][amount]=solve(i,amount-coins[i],coins,n,dp)+solve(i+1,amount,coins,n,dp);
+        if(coins[i]>amount)
+        return dp[i][amount]=ways(i-1,amount,coins,dp);
+        else{
+            return dp[i][amount]=ways(i,amount-coins[i],coins,dp)+ways(i-1,amount,coins,dp);
+        }
     }
     int change(int amount, vector<int>& coins) {
-     int n=coins.size();
-             vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-
-     return solve(0,amount,coins,n,dp);
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        return ways(n-1,amount,coins,dp);
     }
 };
