401d4192735b31979e58122adb844bf270d8edac
somya-singh766 <Somyasingh3007@gmail.com>
2025-11-26 09:41:17 +0530

    Add solution for Climbing Stairs

--- LeetCode/Climbing-Stairs/Solution.cpp
+++ LeetCode/Climbing-Stairs/Solution.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-
+    int ways(int n,vector<int>&dp)
+    {
+      if(n<=1)
+      return 1;
+      if(dp[n]!=-1)
+      return dp[n];
+      return dp[n]=ways(n-1,dp)+ways(n-2,dp);
+    }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        dp[0]=dp[1]=1;
-        for(int i=2;i<=n;i++)
-        {
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+       return ways(n,dp);
     }
 };
