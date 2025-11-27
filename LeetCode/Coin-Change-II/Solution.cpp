2390e188f7a34b519a30183d7c37564ea733ff33
somya-singh766 <Somyasingh3007@gmail.com>
2025-11-27 10:09:09 +0530

    Add solution for Coin Change II

--- LeetCode/Coin-Change-II/Solution.cpp
+++ LeetCode/Coin-Change-II/Solution.cpp
@@ -1,23 +1,30 @@
 1class Solution {
 2public:
 3
-4    int ways(int i,int amount,vector<int>& coins,vector<vector<int>>&dp)
-5    {
-6        if(i==0)
-7        return 0;
-8        if(amount==0)
-9        return 1;
-10        if(dp[i][amount]!=-1)
-11        return dp[i][amount];
-12        if(coins[i-1]>amount)
-13        return dp[i][amount]=ways(i-1,amount,coins,dp);
-14        else{
-15            return dp[i][amount]=ways(i,amount-coins[i-1],coins,dp)+ways(i-1,amount,coins,dp);
-16        }
-17    }
-18    int change(int amount, vector<int>& coins) {
-19        int n=coins.size();
-20        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-21        return ways(n,amount,coins,dp);
-22    }
-23};
+4    
+5    int change(int amount, vector<int>& coins) {
+6        int n=coins.size();
+7         long long MOD = 1e9 + 7;
+8
+9        vector<vector<long long>>dp(n+1,vector<long long>(amount+1,0));
+10        for(int i=0;i<=n;i++)
+11        {
+12            dp[i][0]=1;
+13        }
+14        for(int i=1;i<=n;i++)
+15        {
+16            for(int j=1;j<=amount;j++)
+17            {
+18                if(coins[i-1]>j)
+19                {
+20                  dp[i][j]=dp[i-1][j];
+21
+22                }
+23                else{
+24                   dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j] % MOD; 
+25                }
+26            }
+27        }
+28        return dp[n][amount];
+29    }
+30};
