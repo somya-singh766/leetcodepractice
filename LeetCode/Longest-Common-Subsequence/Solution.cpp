6c7c65ce52c0933190100bda7679c73830be182a
somya-singh766 <Somyasingh3007@gmail.com>
2025-11-27 14:44:46 +0530

    Add solution for Longest Common Subsequence

--- LeetCode/Longest-Common-Subsequence/Solution.cpp
+++ LeetCode/Longest-Common-Subsequence/Solution.cpp
@@ -4,21 +4,25 @@
 4    int longestCommonSubsequence(string text1, string text2) {
 5         int n=text1.size();
 6        int m=text2.size();
-7        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+7         vector<int>dp(m+1);
 8        for(int i=1;i<=n;i++)
 9        {
-10            for(int j=1;j<=m;j++)
-11            {
-12                if(text1[i-1]==text2[j-1])
-13               {
-14                 dp[i][j]= 1+dp[i-1][j-1];
-15               }
-16               else
+10            int curr=0,prev=0;
+11            //prev=dp[i-1][j-1]
+12            for(int j=1;j<=m;j++)
+13            {
+14                prev=curr;
+15                curr=dp[j];
+16                if(text1[i-1]==text2[j-1])
 17               {
-18                   dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-19                }
-20            }
-21        }
-22        return dp[n][m];
-23    }
-24};
+18                 dp[j]= 1+prev;
+19               }
+20               else
+21               {
+22                   dp[j]=max(dp[j],dp[j-1]);
+23                }
+24            }
+25        }
+26        return dp[m];
+27    }
+28};
