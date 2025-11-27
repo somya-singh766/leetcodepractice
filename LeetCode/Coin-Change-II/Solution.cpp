23749c0ad36a53f2ab4195a0683af86ff13dd323
somya-singh766 <Somyasingh3007@gmail.com>
2025-11-27 09:40:36 +0530

    Add solution for Coin Change II

--- LeetCode/Coin-Change-II/Solution.cpp
+++ LeetCode/Coin-Change-II/Solution.cpp
@@ -1,23 +1,23 @@
-class Solution {
-public:
-
-    int ways(int i,int amount,vector<int>& coins,vector<vector<int>>&dp)
-    {
-        if(i<0)
-        return 0;
-        if(amount==0)
-        return 1;
-        if(dp[i][amount]!=-1)
-        return dp[i][amount];
-        if(coins[i]>amount)
-        return dp[i][amount]=ways(i-1,amount,coins,dp);
-        else{
-            return dp[i][amount]=ways(i,amount-coins[i],coins,dp)+ways(i-1,amount,coins,dp);
-        }
-    }
-    int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return ways(n-1,amount,coins,dp);
-    }
-};
+1class Solution {
+2public:
+3
+4    int ways(int i,int amount,vector<int>& coins,vector<vector<int>>&dp)
+5    {
+6        if(i<0)
+7        return 0;
+8        if(amount==0)
+9        return 1;
+10        if(dp[i][amount]!=-1)
+11        return dp[i][amount];
+12        if(coins[i]>amount)
+13        return dp[i][amount]=ways(i-1,amount,coins,dp);
+14        else{
+15            return dp[i][amount]=ways(i,amount-coins[i],coins,dp)+ways(i-1,amount,coins,dp);
+16        }
+17    }
+18    int change(int amount, vector<int>& coins) {
+19        int n=coins.size();
+20        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+21        return ways(n-1,amount,coins,dp);
+22    }
+23};
