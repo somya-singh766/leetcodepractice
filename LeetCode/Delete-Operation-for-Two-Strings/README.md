e40098d6fcddcf9095ceaa430c0cb583af9b9175
somya-singh766 <Somyasingh3007@gmail.com>
2025-11-28 10:07:08 +0530

    Add solution for Delete Operation for Two Strings

---
+++ LeetCode/Delete-Operation-for-Two-Strings/README.md
@@ -0,0 +1,21 @@
+# Delete Operation for Two Strings
+
+Can you solve this real interview question? Delete Operation for Two Strings - Given two strings word1 and word2, return the minimum number of steps required to make word1 and word2 the same.
+
+In one step, you can delete exactly one character in either string.
+
+ 
+
+Example 1:
+
+
+Input: word1 = "sea", word2 = "eat"
+Output: 2
+Explanation: You need one step to make "sea" to "ea" and another step to make "eat" to "ea".
+
+
+Example 2:
+
+
+Input: word1 = "leetcode", word2 = "etco"
+Output: 4
---
+++ LeetCode/Delete-Operation-for-Two-Strings/Solution.cpp
@@ -0,0 +1,24 @@
+1class Solution {
+2public:
+3 int find(int n,int m,string &s1, string &s2, vector<vector<int>>&dp)
+4    {
+5        if(n==0 || m==0)
+6        return 0;
+7        if(dp[n][m]!=-1)
+8        return dp[n][m];
+9        if(s1[n-1]==s2[m-1])
+10        {
+11            return dp[n][m]= 1+find(n-1,m-1,s1,s2,dp);
+12        }
+13        else{
+14            return dp[n][m]=max(find(n-1,m,s1,s2,dp),find(n,m-1,s1,s2,dp));
+15        }
+16    }
+17    int minDistance(string word1, string word2) {
+18         int n=word1.size();
+19        int m=word2.size();
+20        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+21        int lcs= find(n,m,word1,word2,dp);
+22        return n+m-(2*lcs);
+23    }
+24};
