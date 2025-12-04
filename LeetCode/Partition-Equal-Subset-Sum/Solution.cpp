3dad2ad074cb3e400734483bcc8e87bc80a780b6
somya-singh766 <Somyasingh3007@gmail.com>
2025-12-04 13:33:52 +0530

    Add solution for Partition Equal Subset Sum

--- LeetCode/Partition-Equal-Subset-Sum/Solution.cpp
+++ LeetCode/Partition-Equal-Subset-Sum/Solution.cpp
@@ -1,30 +1,24 @@
-1class Solution {
-2public:
-3 bool find(int i,int n,vector<int>&arr,int target,vector<vector<int>>&dp)
-4    {
-5         if(target==0)
-6        return 1;
-7        if(i>=n || target<0)
-8        return 0;
-9        if(dp[i][target]!=-1)
-10        return dp[i][target];
-11
-12       
-13        return dp[i][target]=find(i+1,n,arr,target-arr[i],dp) || find(i+1,n,arr,target,dp);
-14    }
-15    bool canPartition(vector<int>& nums) {
-16        int n=nums.size();
-17        int sum=0,target;
-18        for(int i=0;i<n;i++)
-19        {
-20            sum+=nums[i];
-21        }
-22        if(sum%2!=0)
-23        return 0;
-24        else{
-25            target=sum/2;
-26        }
-27        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
-28        return find(0,n,nums,target,dp);
-29    }
-30};
+    if(target % 2 != 0)
+    {
+        return false;
+    }
+    target /= 2;
+
+    
+    int n = target + 1;
+    bool[] possible = new bool[n];
+    possible[0] = true;
+
+    foreach(int num in nums)
+    {
+        for(int i=n-1; i >= 0; i--)
+        {
+            if(possible[i] && i + num < n)
+            {
+                possible[i + num] = true;
+            }
+        }
+    }
+
+    return possible[target];
+}
