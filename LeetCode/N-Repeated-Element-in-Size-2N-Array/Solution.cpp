46207233223374ed004204e6658a066aff2c1b88
somya-singh766 <Somyasingh3007@gmail.com>
2026-01-02 21:34:00 +0530

    Add solution for N-Repeated Element in Size 2N Array

--- LeetCode/N-Repeated-Element-in-Size-2N-Array/Solution.cpp
+++ LeetCode/N-Repeated-Element-in-Size-2N-Array/Solution.cpp
@@ -2,21 +2,13 @@
 2public:
 3    int repeatedNTimes(vector<int>& nums) {
 4        int m=nums.size();
-5        sort(nums.begin(),nums.end());
-6        int n=m/2;
-7        int count=1;
-8        int i;
-9        for( i=0;i<m;i++)
-10        {
-11            if(nums[i]==nums[i+1])
-12            {
-13                count++;
-14            }
-15            if(count==n)
-16            {
-17                break;
-18            }
-19        }
-20        return nums[i];
-21    }
-22};
+5        for(int i=0;i<m-2;i++)
+6        {
+7            if(nums[i]==nums[i+1] || nums[i]==nums[i+2])
+8            {
+9                return nums[i];
+10            }
+11        }
+12        return nums[m-1];
+13    }
+14};
